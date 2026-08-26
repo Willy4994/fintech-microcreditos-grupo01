436a0ee3807eb192feb8c83ea46807df8a4b1641
Melannie Lorenzana <melannielorenzana24@gmail.com>
2026-08-25 21:52:59 -0600

    Realice cambios

--- src/dominio/cierres/services/CalculdoraCarteraRiesgo.ts
+++ src/dominio/cierres/services/CalculdoraCarteraRiesgo.ts
@@ -3,7 +3,6 @@ import { Cartera } from "../entities/Cartera.js";
 export class CalculadoraCarteraRiesgo {
 
   calcular(cartera: Cartera): void {
-    // Aquí implementaremos las reglas
-    // de cálculo de riesgo de cartera.
+    
   }
 }
