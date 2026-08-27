4c7fd85dae12a45b4f0efb049fdd28e62ff94735
Melannie Lorenzana <melannielorenzana24@gmail.com>
2026-08-26 21:05:13 -0600

    Se modifico la clase CalculadoraCarteraRiesgo

--- src/dominio/cierres/services/CalculdoraCarteraRiesgo.ts
+++ src/dominio/cierres/services/CalculdoraCarteraRiesgo.ts
@@ -3,6 +3,6 @@ import { Cartera } from "../entities/Cartera.js";
 export class CalculadoraCarteraRiesgo {
 
   calcular(cartera: Cartera): void {
-    
+    //Aqui se va a implementar las reglas de cálculo de riesgo de cartera en el siguiente entregable.
   }
 }
